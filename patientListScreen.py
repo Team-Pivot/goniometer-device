e48a2b4387cf9ee69ddfbca401c2cc7154ae5e81
Jeffrey Wongo <wongojeffrey@gmail.com>
2020-04-10 19:50:01 -0400

    added listeners for patient list buttons

--- patientListScreen.py
+++ patientListScreen.py
@@ -23,12 +23,12 @@ class PatientList:
 
     def populateUI(self, response):
         #test values
-        response.append({'firstName':'Jeff', 'lastName':'Wongo'})
-        response.append({'firstName':'Alan', 'lastName':'Watts'})
-        response.append({'firstName':'Dan', 'lastName':'Dude'})
-        response.append({'firstName':'Dan', 'lastName':'Dude'})
-        response.append({'firstName':'Dan', 'lastName':'Dude'})
-        response.append({'firstName':'Dan', 'lastName':'Dude'})
+        # response.append({'firstName':'Jeff', 'lastName':'Wongo'})
+        # response.append({'firstName':'Alan', 'lastName':'Watts'})
+        # response.append({'firstName':'Dan', 'lastName':'Dude'})
+        # response.append({'firstName':'Dan', 'lastName':'Dude'})
+        # response.append({'firstName':'Dan', 'lastName':'Dude'})
+        # response.append({'firstName':'Dan', 'lastName':'Dude'})
         d = {}
         y = 0
         window = self.window
@@ -39,7 +39,6 @@ class PatientList:
                 y += 60
             # print(response[i])
             d['button{}'.format(i)] = QtWidgets.QPushButton()
-            d['button{}'.format(i)] = QtWidgets.QPushButton()
             d['button{}'.format(i)].setGeometry(QtCore.QRect(30, y, 351, 41))
             font = QtGui.QFont()
             font.setPointSize(10)
@@ -48,8 +47,14 @@ class PatientList:
             _translate = QtCore.QCoreApplication.translate
             d['button{}'.format(i)].setText(_translate("MainWindow", name))
             window.scrollLayout.addWidget(d['button{}'.format(i)])
+            id = response[i]['id']
+            d['button{}'.format(i)].clicked.connect(lambda: self.makeSession(id))
             d['button{}'.format(i)].show()
         print(len(d))
         for val in d.values():
             print(val.parent())
             val.show()
+
+    def makeSession(self, patientId):
+        self.window.patientId = patientId
+        self.window.openJoints()
