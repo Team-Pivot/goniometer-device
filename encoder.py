dcc0b470779bc7da5eabb3dd271525b4393ac098
Jeffrey Wongo <wongojeffrey@gmail.com>
2020-04-13 14:06:16 -0400

    small fix

--- encoder.py
+++ encoder.py
@@ -51,7 +51,7 @@ class Encoder:
 				if GPIO.event_detected(btn):
 					angle = 0
 					print("angle: {}".format(angle))
-					strAngle = angle + ''
+					strAngle = str(angle)
 					self.window.angle_2.setText(_translate("MainWindow", strAngle))
 				if clkState != clkLastState:
 					dtState = GPIO.input(dt)
@@ -60,7 +60,7 @@ class Encoder:
 					else:
 						angle += 9
 					print("angle: {}".format(angle))
-					strAngle = angle + ''
+					strAngle = str(angle)
 					self.window.angle_2.setText(_translate("MainWindow", strAngle))
 				self.angle = angle
 				clkLastState = clkState
